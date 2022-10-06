d8aa6ec70aa6e4fe757c5a3889f8a7aba42dcb32
DanielRoodzantHU <daniel.roodzant@student.hu.nl>
2022-10-06 11:44:29 +0200

    Added new user and book functionalities

--- books.js
+++ books.js
@@ -1,8 +1,8 @@
 function registerBook() {
     // Formulier uitlezen
-    let titleInvoer = document.getElementById('title').value;
-    let authorInvoer = document.getElementById('author').value;
-    let isbnInvoer = document.getElementById('isbn').value;
+    let titleInvoer = document.getElementById('titleReg').value;
+    let authorInvoer = document.getElementById('authorReg').value;
+    let isbnInvoer = document.getElementById('isbnReg').value;
 
     let newBook = {
         title: titleInvoer,
@@ -21,7 +21,89 @@ function registerBook() {
         alert('Book registered');
     })
     .catch(error => {
-        alert('Could not register Book. Please check input.');
+        alert('Could not register book. Please check input.');
     });
 }
 
+function deleteBook() {
+    // Formulier uitlezen
+    let bookIdInvoer = document.getElementById('bookIdDel').value;
+
+
+    fetch("http://localhost:8080/book/delete/"+bookIdInvoer, {
+        method: 'DELETE'
+        
+    })
+    .then(response => {
+        alert('Book deleted');
+    })
+    .catch(error => {
+        alert('Could not delete book. Please check input.');
+    });
+}
+
+function updateBook() {
+    // Formulier uitlezen
+    let bookIdInvoer = document.getElementById('bookIdUpd').value;
+    let titleInvoer = document.getElementById('titleUpd').value;
+    let authorInvoer = document.getElementById('authorUpd').value;
+    let isbnInvoer = document.getElementById('isbnUpd').value;
+
+    let newBook = {
+        title: titleInvoer,
+        author: authorInvoer,
+        isbn: isbnInvoer
+    }
+
+    fetch("http://localhost:8080/book/update/"+bookIdInvoer, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newBook)
+    })
+    .then(response => {
+        alert('Book updated');
+    })
+    .catch(error => {
+        alert('Could not update book. Please check input.');
+    });
+}
+
+function show(data) {
+    let bookTableHtml =
+        `<tr>
+                <th>ID</th>
+                <th>Titel</th>
+                <th>Auteur</th>
+                <th>ISBN</th>
+             </tr>`;
+
+    // Loop to access all rows 
+    for (let r of data) {
+        bookTableHtml += `<tr> 
+                <td>${r.bookId} </td>
+                <td>${r.title}</td>
+                <td>${r.author}</td>
+                <td>${r.isbn}</td>
+            </tr>`;
+    }
+
+    // Setting innerHTML as tab variable
+    document.getElementById("books").innerHTML = bookTableHtml;
+}
+
+function getapi() {
+    fetch("http://localhost:8080/book")
+        .then(response => response.json())
+        .then(a => {
+            console.log('response', a);
+            show(a);
+        })
+        .catch(error => {
+            console.log('error', error);
+            // handle the error
+        });
+}
+
+getapi();
--- users.js
+++ users.js
@@ -1,11 +1,10 @@
 function registerUser() {
     // Formulier uitlezen
-    let firstNameInvoer = document.getElementById('firstName').value;
-    let lastNameInvoer = document.getElementById('lastName').value;
-    let emailInvoer = document.getElementById('email').value;
-    let passwordInvoer = document.getElementById('password').value;
-    let adminInvoer = document.getElementById('admin').checked;
-
+    let firstNameInvoer = document.getElementById('firstNameReg').value;
+    let lastNameInvoer = document.getElementById('lastNameReg').value;
+    let emailInvoer = document.getElementById('emailReg').value;
+    let passwordInvoer = document.getElementById('passwordReg').value;
+    let adminInvoer = document.getElementById('adminReg').checked;
 
     let newUser = {
         firstName: firstNameInvoer,
@@ -32,8 +31,7 @@ function registerUser() {
 
 function deleteUser() {
     // Formulier uitlezen
-    let userIdInvoer = document.getElementById('userId').value;
-
+    let userIdInvoer = document.getElementById('userIdDel').value;
 
     fetch("http://localhost:8080/user/delete/"+userIdInvoer, {
         method: 'DELETE'
@@ -46,3 +44,77 @@ function deleteUser() {
         alert('Could not delete user. Please check input.');
     });
 }
+
+function changeUser() {
+    // Formulier uitlezen
+    let changeUserIdInvoer = document.getElementById('userIdCha').value;
+    let firstNameInvoer = document.getElementById('firstNameCha').value;
+    let lastNameInvoer = document.getElementById('lastNameCha').value;
+    let emailInvoer = document.getElementById('emailCha').value;
+    let passwordInvoer = document.getElementById('passwordCha').value;
+    let adminInvoer = document.getElementById('adminCha').checked;
+
+    let changeUserInfo = {
+        firstName: firstNameInvoer,
+        lastName: lastNameInvoer,
+        email: emailInvoer,
+        password: passwordInvoer,
+        admin: adminInvoer
+    }
+
+    fetch("http://localhost:8080/user/update/"+changeUserIdInvoer, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(changeUserInfo)
+    })
+    .then(response => {
+        alert('User changed');
+    })
+    .catch(error => {
+        alert('Could not change user. Please check input.');
+    });
+}
+
+function show(data) {
+    let userTableHtml =
+        `<tr>
+                <th>ID</th>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Admin?</th>
+             </tr>`;
+
+    // Loop to access all rows 
+    for (let r of data) {
+        userTableHtml += `<tr> 
+                <td>${r.userId} </td>
+                <td>${r.firstName}</td>
+                <td>${r.lastName}</td>
+                <td>${r.email}</td>
+                <td>${r.password}</td>
+                <td>${r.admin}</td>
+            </tr>`;
+    }
+
+    // Setting innerHTML as tab variable
+    document.getElementById("users").innerHTML = userTableHtml;
+}
+
+function getapi() {
+    fetch("http://localhost:8080/user")
+        .then(response => response.json())
+        .then(a => {
+            console.log('response', a);
+            show(a);
+        })
+        .catch(error => {
+            console.log('error', error);
+            // handle the error
+        });
+}
+
+getapi();
